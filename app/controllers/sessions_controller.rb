42cf4648adf2023fbc5a0b22288f30025bd44bb5
ManishaHaridas <manisha@qburst.com>
2013-04-23 16:05:02 +0530

    Sign-In using devise

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,5 @@
+class SessionsController < Devise::SessionsController
+#def redirect_to_new_user_session
+#redirect_to new_user_session
+#end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ActionController::Base
+def index
+redirect_to new_user_session_path
+end
+end
