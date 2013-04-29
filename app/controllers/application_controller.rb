de2a8d5c5ac21ff12fc89e37cebcaa0ca33fdf06
ManishaHaridas <manisha@qburst.com>
2013-04-29 09:10:29 +0530

    Profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :layout
+
+  private
+
+  def layout
+    # only turn it off for login pages:
+    is_a?(Devise::SessionsController) ? false : "application"
+    
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ActionController::Base
 def index
-redirect_to new_user_session_path, :layout=> false
-
+	redirect_to new_user_session_path 
 end
 end
